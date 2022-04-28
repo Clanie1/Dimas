d2477cbaae1eb512962bec958b886bc33cde2c4f
Clanie1 <danybarociov@gmail.com>
2022-04-28 10:53:53 -0700

    header

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const app = ()=>{
-    const burger = document.getElementById("burger"); 
-    const menu = document.querySelector(".header_list");
-    console.log(menu.innerHTML);
-    burger.addEventListener("click", ()=>{
-        menu.classList.toggle("menu_active");
-        burger.classList.toggle("toggle");
-    });
-}
+const app = () => {
+  const burger = document.getElementById("burger");
+  const menu = document.querySelector(".header_list");
+  console.log(menu.innerHTML);
+  burger.addEventListener("click", () => {
+    menu.classList.toggle("menu_active");
+    burger.classList.toggle("toggle");
+  });
+};
 app();
+
+const handleScroll = (query) => {
+  document.getElementById(query).scrollIntoView({ behavior: "smooth" });
+};
