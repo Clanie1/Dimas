ed330f466f787197a68aeba072930e4d405e0f87
Clanie1 <danybarociov@gmail.com>
2022-04-29 23:10:11 -0700

    nueva libreria

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ const app = () => {
   });
 };
 app();
+new Glider(document.querySelector(".glider"), {
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  draggable: true,
+  dots: ".dots",
+  arrows: {
+    prev: ".glider-prev",
+    next: ".glider-next",
+  },
+});
 
 const handleScroll = (query) => {
   document.getElementById(query).scrollIntoView({ behavior: "smooth" });
