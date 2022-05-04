b45fb123970276a43d4fb13bd890de6cdcd30b77
Daniel Barocio <danybarociov@gmail.com>
2022-05-03 22:37:43 -0700

    commit con gitbash

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ app();
 new Glider(document.querySelector(".glider"), {
   slidesToShow: 2,
   slidesToScroll: 1,
-  draggable: true,
   dots: ".dots",
+  draggable: true,
   arrows: {
     prev: ".glider-prev",
     next: ".glider-next",
