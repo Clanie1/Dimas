e3867c323c137579dcda9fe2aacc74be7364a6b9
Daniel Barocio <danybarociov@gmail.com>
2022-05-08 18:12:53 -0700

    navegacion arreglada

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-const app = () => {
-  const burger = document.getElementById("burger");
-  const menu = document.querySelector(".header_list");
-  const more = document.querySelector(".ver_button");
-  const section = document.querySelector(".section_vermas");
+const burger = document.getElementById("burger");
+const menu = document.querySelector(".header_list");
+const more = document.querySelector(".ver_button");
+const section = document.querySelector(".section_vermas");
 
-  burger.addEventListener("click", () => {
-    menu.classList.toggle("menu_active");
-    burger.classList.toggle("toggle");
-  });
-
-  let flag = false;
-  more.addEventListener("click", () => {
-    section.classList.toggle("option");
-    if (!flag) {
-      more.innerHTML = "Ver menos";
-    } else {
-      more.innerHTML = "Ver más";
-    }
-    flag = !flag;
-  });
-
-  function handleScroll(query) {
-    document.getElementById(query).scrollIntoView({ behavior: "smooth" });
-  }
+window.handleScroll = function handleScroll(query) {
+  document.getElementById(query).scrollIntoView({ behavior: "smooth" });
 };
 
-app();
+burger.addEventListener("click", () => {
+  menu.classList.toggle("menu_active");
+  burger.classList.toggle("toggle");
+});
+
+let flag = false;
+more.addEventListener("click", () => {
+  section.classList.toggle("option");
+  if (!flag) {
+    more.innerHTML = "Ver menos";
+  } else {
+    more.innerHTML = "Ver más";
+  }
+  flag = !flag;
+});
